7123d25f43486177dae7bfcf75e13251dfe7eeb8
Knipl Gergely <knipl.gergely@greenformatics.hu>
2021-12-06 16:20:38 +0100

    converting directory menu component to class from a function

--- src/components/directory-menu/directory-menu.component.js
+++ src/components/directory-menu/directory-menu.component.js
@@ -1,16 +1,26 @@
+import { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory-menu.styles.scss"
 
-function DirectoryMenu() {
-  return (
-    <div className="directory-menu">
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-        <MenuItem />
-      </div>
-  )
+class DirectoryMenu extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  render() {
+    return(
+      <div className="directory-menu">
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+          <MenuItem />
+        </div>
+    )
+  }
+
 }
 
 export default DirectoryMenu;
